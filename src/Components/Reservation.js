04d0ad86940b7ef02f30a700e9b246eb4d0380e1
Eileen <eileenburdekin@proton.me>
2023-12-07 20:29:21 -0800

    added alt text to table images

--- src/Components/Reservation.js
+++ src/Components/Reservation.js
@@ -6,7 +6,7 @@ export default function Reservation({reservation, handleDelete, handleEdit}) {
 
     return <>
     <tr>
-        <td><img src="../images/table.png" width="20px" /></td>
+        <td><img src="../images/table.png" width="20px" alt="table" /></td>
        <td>{reservation.name}</td>
        <td>{reservation.party}</td>
        <td>{reservation.time}</td>
