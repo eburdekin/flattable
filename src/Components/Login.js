9384f0a357c7fa68f143d5dce567c94ca145da44
Eileen <eileenburdekin@proton.me>
2023-12-07 21:03:49 -0800

    changed password input from text to password field

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -54,7 +54,7 @@ export default function Login() {
             <div className="inputContainer">
                 <label>Password</label>
                 <input
-                    type='text'
+                    type='password'
                     className='loginInput'
                     placeholder="Password"
                     style={style}
