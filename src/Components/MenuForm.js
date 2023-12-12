c38f12eeb972eeae343c97bc49f6ff2425d1a2c7
Eileen <eileenburdekin@proton.me>
2023-12-11 16:18:22 -0800

    change Menu functionality from form to clickable menu items

--- src/Components/MenuForm.js
+++ src/Components/MenuForm.js
@@ -1,90 +1,97 @@
-import React, { useState } from 'react'
+// import React, { useState } from "react";
 
-export default function MenuForm({ menu, updateMenu }) {
+// export default function MenuForm({ menu, updateMenu }) {
+//   const [newMenu, setNewMenu] = useState(menu);
 
-    const [newMenu, setNewMenu] = useState(menu)
+//   const handleChange = (e) => {
+//     const name = e.target.name;
+//     let value = e.target.value;
+//     setNewMenu({ ...newMenu, [name]: value });
+//   };
 
-    const handleChange = (e) => {
-        const name = e.target.name;
-        let value = e.target.value
-        setNewMenu({ ...newMenu, [name]: value })
-    }
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     updateMenu(newMenu);
+//   };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        updateMenu(newMenu)
-    }
+//   const style = {
+//     fontFamily: "Roboto",
+//     padding: "2px",
+//     margin: "10px",
+//     width: "200px",
+//   };
 
-    const style = {
-        'fontFamily': 'Roboto',
-        'padding': '2px',
-        'margin': '10px',
-        'width': '200px',
-    }
+//   const buttonStyle = {
+//     fontFamily: "Roboto",
+//     padding: "8px",
+//     margin: "5px",
+//     backgroundColor: "#BF41B7",
+//     color: "white",
+//     fontSize: "14px",
+//     fontWeight: "bold",
+//     borderRadius: "15px",
+//   };
 
-    const buttonStyle = {
-        'fontFamily': 'Roboto',
-        'padding': '8px',
-        'margin': '5px',
-        'backgroundColor': '#BF41B7',
-        'color': 'white',
-        'fontSize': '14px',
-        'fontWeight': 'bold',
-        'borderRadius': '15px'
-    }
-
-    return <>
-        <form className="menuForm" onSubmit={handleSubmit}>
-            <label>Breakfast</label><br></br>
-            <input
-                type="text"
-                placeholder="Breakfast"
-                name="Breakfast"
-                value={menu.Breakfast || ""}
-                onChange={handleChange}
-                style={style}
-            ></input><br></br>
-            <label>Lunch</label><br></br>
-            <input
-                type="text"
-                placeholder="Lunch"
-                name="Lunch"
-                value={menu.Lunch || ""}
-                onChange={handleChange}
-                style={style}
-            ></input><br></br>
-            <label>Dinner</label><br></br>
-            <input
-                type="text"
-                placeholder="Dinner"
-                name="Dinner"
-                value={menu.Dinner || ""}
-                onChange={handleChange}
-                style={style}
-            ></input><br></br>
-            <label>Dessert</label><br></br>
-            <input
-                type="text"
-                placeholder="Dessert"
-                name="Dessert"
-                value={menu.Dessert || ""}
-                onChange={handleChange}
-                style={style}
-            ></input><br></br>
-            <label>Special</label><br></br>
-            <input
-                type="text"
-                placeholder="Special"
-                name="Special"
-                value={menu.Special || ""}
-                onChange={handleChange}
-                style={style} ></input>
-            <br></br>
-            <input
-                type="submit"
-                value="Update Menu"
-                style={buttonStyle}
-            />
-        </form >
-    </>
-}
+//   return (
+//     <>
+//       <form className="menuForm" onSubmit={handleSubmit}>
+//         <label>Breakfast</label>
+//         <br></br>
+//         <input
+//           type="text"
+//           placeholder="Breakfast"
+//           name="Breakfast"
+//           value={menu.Breakfast || ""}
+//           onChange={handleChange}
+//           style={style}
+//         ></input>
+//         <br></br>
+//         <label>Lunch</label>
+//         <br></br>
+//         <input
+//           type="text"
+//           placeholder="Lunch"
+//           name="Lunch"
+//           value={menu.Lunch || ""}
+//           onChange={handleChange}
+//           style={style}
+//         ></input>
+//         <br></br>
+//         <label>Dinner</label>
+//         <br></br>
+//         <input
+//           type="text"
+//           placeholder="Dinner"
+//           name="Dinner"
+//           value={menu.Dinner || ""}
+//           onChange={handleChange}
+//           style={style}
+//         ></input>
+//         <br></br>
+//         <label>Dessert</label>
+//         <br></br>
+//         <input
+//           type="text"
+//           placeholder="Dessert"
+//           name="Dessert"
+//           value={menu.Dessert || ""}
+//           onChange={handleChange}
+//           style={style}
+//         ></input>
+//         <br></br>
+//         <label>Special</label>
+//         <br></br>
+//         <input
+//           type="text"
+//           placeholder="Special"
+//           name="Special"
+//           value={menu.Special || ""}
+//           onChange={handleChange}
+//           style={style}
+//         ></input>
+//         <br></br>
+//         <input type="submit" value="Update Menu" style={buttonStyle} />
+//       </form>
+//     </>
+//   );
+// }
--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -1,52 +1,105 @@
-import '../App.css';
-import { useState, useEffect } from 'react'
-import Header from '../Components/Header'
-import NavBar from "../Components/NavBar"
-import MenuForm from "../Components/MenuForm"
+import "../App.css";
+import { useState, useEffect } from "react";
+import Header from "../Components/Header";
+import NavBar from "../Components/NavBar";
+import MenuForm from "../Components/MenuForm";
 
-const API = 'http://localhost:8001/menu'
+const API = "http://localhost:8001/menu";
 
 export default function Menu() {
+  const [menu, setMenu] = useState({});
 
-    const [menu, setMenu] = useState({})
+  const [selectedField, setSelectedField] = useState(null);
 
-    useEffect(() => {
-        fetch(API)
-            .then(r => r.json())
-            .then(setMenu)
-    }, [])
+  useEffect(() => {
+    fetch(API)
+      .then((r) => r.json())
+      .then(setMenu);
+  }, []);
 
-    function updateMenu(newMenu) {
-        fetch(API, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(newMenu)
-        })
-        setMenu(newMenu)
-    }
+  function handleUpdate(newMenu) {
+    fetch(API, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newMenu),
+    });
+    setMenu(newMenu);
+  }
 
-    const current = new Date();
-    const date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`;
+  const current = new Date();
+  const date = `${current.getDate()}/${
+    current.getMonth() + 1
+  }/${current.getFullYear()}`;
 
+  const buttonStyle = {
+    fontFamily: "Roboto",
+    padding: "6px",
+    backgroundColor: "#BF41B7",
+    color: "white",
+    fontSize: "12px",
+    fontWeight: "bold",
+    borderRadius: "12px",
+  };
 
-    return <>
-        <main>
-            <Header />
-            <br />
-            <NavBar />
-        </main>
-        <div className="mainComponent">
-            <h2>Today's Menu</h2>
-            <h3>{date}</h3>
-            <div className="menu">
-                {Object.entries(menu).map(([course, mealName]) => <><h4>{course}</h4><p>{mealName}</p></>)}
-            </div>
-            <br></br>
-            <h3>Update Menu</h3>
-            <MenuForm menu={menu} updateMenu={updateMenu} />
-            <br></br>
+  return (
+    <>
+      <main>
+        <Header />
+        <br />
+        <NavBar />
+      </main>
+      <div className="mainComponent">
+        <h2>Today's Menu</h2>
+        <h3>{date}</h3>
+        <div className="menu">
+          {Object.entries(menu).map(([course, mealName]) => (
+            <>
+              <h4>{course}</h4>
+              {selectedField === course ? (
+                <>
+                  <input
+                    type="text"
+                    value={mealName}
+                    onChange={(e) => {
+                      const updatedMenu = { ...menu };
+                      updatedMenu[course] = e.target.value;
+                      handleUpdate(updatedMenu);
+                    }}
+                  />
+                  <button
+                    style={buttonStyle}
+                    onClick={() => setSelectedField(course)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    style={buttonStyle}
+                    onClick={() => setSelectedField(!selectedField)}
+                  >
+                    Done/Cancel
+                  </button>
+                </>
+              ) : (
+                <>
+                  <p>{mealName}</p>
+                  <button
+                    style={buttonStyle}
+                    onClick={() => setSelectedField(course)}
+                  >
+                    Edit
+                  </button>
+                </>
+              )}
+            </>
+          ))}
         </div>
+        <br></br>
+        {/* <h3>Update Menu</h3>
+            <MenuForm menu={menu} updateMenu={updateMenu} /> */}
+        <br></br>
+      </div>
     </>
+  );
 }
